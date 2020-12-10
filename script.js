7d57961ebeb7715ff7370d245604bc21fd11b48b
Nakiyah Fears <fkiyah@yahoo.com>
2020-12-10 15:36:57 -0500

    Initial commit

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+var lettersUpper ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lettersLower = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var specialChar = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~';
+var criteria = '';
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var hasLowerCase = document.getElementById('pwLowerCase').checked;
+var hasUpperCase = document.getElementById('pwUpperCase').checked;
+var hasNumeric = document.getElementById('pwNumeric').checked;
+var hasSpecialChar = document.getElementById('pwSpecialChar').checked;
 
-  passwordText.value = password;
+var pwLength = document.getElementById('pwLength').value;
 
+var generateBtn = document.getElementById('generate');
+var passwordContainer = document.getElementById('password');
+
+//If criteria checkbox is selected... add the associated character string to the 'criteria' variable
+//Return the final 'criteria' string
+function buildString(){
+    if (!hasLowerCase && !hasUpperCase && !hasNumeric && !hasSpecialChar){
+        criteria = '';
+    }
+
+	if (hasLowerCase){
+    	criteria += lettersLower;
+	} 
+
+	if (hasUpperCase){
+    	criteria += lettersUpper;
+	} 
+    
+    if (hasNumeric){
+    	criteria += numbers;
+	} 
+    
+    if (hasSpecialChar){
+    	criteria += specialChar;
+	}
+    
+    return criteria; 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Pass in the length selected by the user (this is being stored in the variable pwLength);
+//Call the buildString function and get the length of the string
+//Do the math to generate the password based on the criteria returned from buildString() and the length chosen by the user
+//Return the result
+function buildPassword(length) {
+	var passwordStr = buildString();
+	var passwordLength = passwordStr.length;
+
+	var result = '';
+
+	for ( let i = 0; i < length; i++ ) {
+		result += passwordStr.charAt(Math.floor(Math.random() * passwordLength));
+	}
+
+	return result;
+}
+
+//Call buildPassword and pass in pwLength
+//Update the innerText of #password with the final password string
+generateBtn.addEventListener('click', function(){
+	var password = buildPassword(pwLength);
+	passwordContainer.innerText = password;
+});
